4ae3c53c13ea632dbf1d304b99a24f430e2d0548
miska689 <mihaiboinceanu2004@gmail.com>
2023-11-18 23:40:30 +0200

    last

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-import requests
-from bs4 import BeautifulSoup
+'''
+    Acest modul contine o functie care poate detecta informatiile
+    dintrun site si titlul ei
+'''
 
-link = input("Introduceti linkul: ")
+import requests # conectarea cu pagina web
+from bs4 import BeautifulSoup # manipularea datelor primite in format html, xml
 
-source = requests.get(link).text # primim raspunsul de la pagina
+def getInfoLink(link: str) -> dict:
+    '''
+        Aceasta functie face parsing la o pagina si 
+        colecteaza titlul paginii si descrierea 
+        din tag-ul <meta>
+        
+        Ca parametrii avem linkul - tip string
+        Ca date de esire avem un dictionar - tip dict
+        
+        {
+            "title": "titlul_paginii"
+            "description": "descrierea paginii"
+        }
+    '''
+    source = requests.get(link).text # primim raspunsul de la pagina
 
-page = BeautifulSoup(source, 'html.parser')  
+    page = BeautifulSoup(source, 'html.parser')  
 
-title_page = page.title.text # titlul paginii
+    title_page = page.title.text # titlul paginii
 
-description = page.find("meta", {"name":"description"}) # descrierea (daca nu exista are valuarea None)
+    description = page.find("meta", {"name":"description"}) # descrierea (daca nu exista are valuarea None)
+    
+    if description: 
+        description = description['content']
+    else:
+        description = "Nu exista"
+    
+    return {'title': title_page, 'description': description}
 
-print("Titul paginii: ", title_page)
 
-print()
+if __name__ == "__main__":
+    link = input("Introduceti linkul:")
 
-if description:
-    print("Descrierea: ", description['content'])
-else:
-    print("Nu exista descriere la pagina!")
+    info = getInfoLink(link)
+
+    print('/////////////////////////////////////////')
+    print("Titlul paginii: ", info['title'])
+    print()
+    print("Descrierea: ", info['description'])
+    print('/////////////////////////////////////////')
+
+
+
+
